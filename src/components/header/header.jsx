75806cf3a7fafb6a33e745a07f8ab217418aff00
adi peled <adipeled224@gmail.com>
2022-01-23 10:47:41 +0200

    create header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { GiHamburgerMenu } from "react-icons/gi";
+
+import './header.css'
+import pigImage from './moneyPig.svg'
+export function Header(props) {
+// check header push
+    const [isOpenModal, setisOpenModal] = useState(false);
+
+    const openAndCloseModal =()=>{
+        setisOpenModal(!isOpenModal);
+    }
+
+    return (
+        <header className='header-cmp'>
+            <div className='logo'></div>
+            <div className="homeicon "></div>
+            <GiHamburgerMenu onClick={openAndCloseModal} className='hamburger' color='white' size={32}/>
+
+            {isOpenModal && <div className="navbar-modal" >
+                <h3 className="show-game-page">Game</h3>
+                <h3 className="show-profile-page">Profile</h3>
+                <h3 className="show-games-results-page">My Games</h3>
+            </div>}
+            
+            <div className="pigMoney">
+                <img src={pigImage}/>
+                <h3>33<span className="moneyAmount"></span>M₩</h3>
+                <h4></h4>
+            </div>
+        </header>
+    );
+}
+
+
+
