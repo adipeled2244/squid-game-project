fd979cb9c3ed90d7fc835c6c5fc38ea17b55afc8
noyShabo <noysh1234@gmail.com>
2022-02-13 10:25:18 +0200

    check

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import { ReactRouter } from './Router/router';
 import {BrowserRouter} from 'react-router-dom';
 
+
+// check
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
