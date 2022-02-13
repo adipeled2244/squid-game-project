504b59bcb35b11013aa7603b9d10fd5299330ce8
adi peled <adipeled224@gmail.com>
2022-02-13 10:44:29 +0200

    navbar close by click on link

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -13,6 +13,9 @@ export function Header({money,user}) {
         setisOpenModal(!isOpenModal);
     }
 
+    const linkInNavbarClicked =()=>{
+        setisOpenModal(false);
+    }
     return (
         <header className='header-cmp'>
             <div className='logo'></div>
@@ -20,15 +23,14 @@ export function Header({money,user}) {
             <GiHamburgerMenu onClick={openAndCloseModal} className='hamburger' color='white' size={32}/>
 
             {isOpenModal && <div className="navbar-modal" >
-                {!user && <NavLink className='link' exact="true" to='/'><h3 className="show-profile-page">Login / Signup</h3></NavLink>}
-                {user && <NavLink className='link' to='/profile'><h3 className="show-profile-page">Profile</h3></NavLink>}
+                {!user && <NavLink onClick={linkInNavbarClicked} className='link' exact="true" to='/'><h3 className="show-profile-page">Login / Signup</h3></NavLink>}
+                {user && <NavLink onClick={linkInNavbarClicked} className='link' to='/profile'><h3 className="show-profile-page">Profile</h3></NavLink>}
 
-                {user && user.color=='blue' && <NavLink className='link' to='/game'><h3 className="show-game-page">Game</h3></NavLink>}
-                {user && user.color=='blue' &&<NavLink className='link' to='/results'> <h3 className="show-games-results-page">My Games</h3></NavLink>}
+                {user && user.color=='blue' && <NavLink onClick={linkInNavbarClicked} className='link' to='/game'><h3 className="show-game-page">Game</h3></NavLink>}
+                {user && user.color=='blue' &&<NavLink onClick={linkInNavbarClicked} className='link' to='/results'> <h3 className="show-games-results-page">My Games</h3></NavLink>}
 
-                {user && user.color=='red'&& <NavLink className='link' to='/users'> <h3 className="show-games-results-page">Players</h3></NavLink>}
-                {user && user.color=='black'&& <NavLink className='link' to='/users'> <h3 className="show-games-results-page">My workers AND My Players</h3></NavLink>}
-
+                {user && user.color=='red'&& <NavLink onClick={linkInNavbarClicked} className='link' to='/users'> <h3 className="show-games-results-page">Players</h3></NavLink>}
+                {user && user.color=='black'&& <NavLink onClick={linkInNavbarClicked} className='link' to='/users'> <h3 className="show-games-results-page">My workers AND My Players</h3></NavLink>}
 
             </div>}
             
